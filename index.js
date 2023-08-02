c003391c14a2d6febe09665c8987a0f10665754d
Wyatt <wyattstephenson@rocketmail.com>
2023-08-02 14:29:08 -0600

    0 JAVA SCRIPT EVENTS ACTING ...

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   const left = parseInt(leftNumbers, 10);
 
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left - 5}px`;
   }
 }
 
@@ -19,6 +19,14 @@ document.addEventListener("keydown", function (e) {
     moveDodgerRight();
   }
 
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  }
+
+  if (e.key === "ArrowDown") {
+   moveDodgerDown()
+  }
+
 });
 
 function moveDodgerRight() {
@@ -26,6 +34,26 @@ function moveDodgerRight() {
   const left = parseInt(leftNumbers, 10);
 
   if (left < 360) {
-    dodger.style.left = `${left + 1}px`;
+    dodger.style.left = `${left + 5}px`;
+  }
+}
+
+// Move the dodger up
+function moveDodgerUp(){
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom < 360 ) {
+    dodger.style.bottom = `${bottom + 5}px`;
+  }
+}
+
+// Move the dodger down
+function moveDodgerDown(){
+  const bottomNumbers = dodger.style.bottom.replace("px", "");
+  const bottom = parseInt(bottomNumbers, 10);
+
+  if (bottom > 0 ) {
+    dodger.style.bottom = `${bottom - 5}px`;
   }
 }
